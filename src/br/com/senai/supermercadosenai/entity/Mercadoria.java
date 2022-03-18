942270956fd578affdd0e58e93d123a72c3f4d2b
Ana Carolina Santos <anacarolina.s.s.hotmail.com>
2022-03-18 20:17:01 -0300

    Inicio da criação dos cadastros de mercadoria e colaborado

---
+++ src/br/com/senai/supermercadosenai/entity/Mercadoria.java
@@ -0,0 +1,27 @@
+package br.com.senai.supermercadosenai.entity;
+
+public class Mercadoria {
+	
+	private Integer id;
+	
+	private String descrição;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescrição() {
+		return descrição;
+	}
+
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+	
+	
+
+}
